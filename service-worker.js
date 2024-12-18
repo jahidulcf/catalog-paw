63e44e15b5fd8f780c7e49beb0b11f2665f2082d
Jahidul <jahidul.cf@gmail.com>
2024-12-19 01:59:40 +0800

    paw copied

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'catalogue-app-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/css/materialize.min.css',
+    '/css/styles.css',
+    '/js/materialize.min.js',
+    '/js/scripts.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Caching assets');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+        )
+    );
+});
