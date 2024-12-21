604d03783e98f217dbea24e4a7f40288cf218a10
Jahidul <jahidul.cf@gmail.com>
2024-12-22 01:22:57 +0800

    adjust back

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const CONSTANTS = {
         defaultHeight: "144px",
-        maxHeight: "400px",
+        maxHeight: "600px",
         debounceDelay: 300,
     };
 
